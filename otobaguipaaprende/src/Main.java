b2931781f843cc5b112a73750d24e2cef8246d47
Henry_ <136041693+Henryqui@users.noreply.github.com>
2024-05-07 08:39:33 -0300

    Update Main.java

--- otobaguipaaprende/src/Main.java
+++ otobaguipaaprende/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         ConversaoDolarReal cdr = new ConversaoDolarReal();
+        DecimalFormat df = new DecimalFormat("##,##0.00")
 
         System.out.println("Qual o valor em dolar que deseja converter? ");
         cdr.valor = sc.nextInt();
@@ -12,7 +13,7 @@ public class Main {
         System.out.println("Qual a cotação do dolar? ");
         cdr.cotad = sc.nextInt();
 
-        int vdc = cdr.ConverterValor();
+        double vdc = cdr.ConverterValor();
 
         System.out.println("O valor dos dolares convertidos para real é de: " + vdc);
 
